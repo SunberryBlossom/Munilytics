d49eaba6646df00b026f75a83d3746a8f021ddce
lskld <theo.lillieskold@gmail.com>
2026-01-26 14:57:25 +0100

    Completed postgres/docker setup in apphost

--- Munilytics.AppHost/AppHost.cs
+++ Munilytics.AppHost/AppHost.cs
@@ -3,8 +3,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 // Postgres setup
 var postgresPassword = builder.AddParameter("postgres-password", "postgres");
 var postgres = builder.AddPostgres("hydra", password: postgresPassword)
+    .WithImageRegistry("ghcr.io")
     .WithImage("hydradatabase/hydra", "latest")
     .WithDataVolume("hydra-data")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase("MunilyticsDb");
 
 var server = builder.AddProject<Projects.Munilytics_Server>("server")
