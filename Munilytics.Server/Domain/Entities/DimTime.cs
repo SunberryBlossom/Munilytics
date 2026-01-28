be4f7713efdd9ad04ce023db1f631d5117de3934
lskld <theo.lillieskold@gmail.com>
2026-01-28 11:07:06 +0100

    Added attributes to the Time Dimension

--- Munilytics.Server/Domain/Entities/DimTime.cs
+++ Munilytics.Server/Domain/Entities/DimTime.cs
@@ -2,5 +2,11 @@
 {
     public class DimTime
     {
+        public int Id { get; set; } 
+        public DateOnly Year { get; set; } 
+        public int Decade { get; set; } 
+        public string MandatePeriod { get; set; } = string.Empty;
+        public bool IsElectionYear { get; set; } 
+        public int RelativeYear { get; set; } //Timespan
     }
 }
