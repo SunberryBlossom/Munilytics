59afd6ba98662128fccff7ad832bbb2d2fe0e848
lskld <theo.lillieskold@gmail.com>
2026-01-28 11:21:56 +0100

    Created attributes for the Municipality dimension

--- Munilytics.Server/Domain/Entities/DimMunicipality.cs
+++ Munilytics.Server/Domain/Entities/DimMunicipality.cs
@@ -2,5 +2,13 @@
 {
     public class DimMunicipality
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string County { get; set; } = string.Empty;
+        public string SKRGroupCode { get; set; } = string.Empty;
+        public string SKRGroupName { get; set; } = string.Empty;
+
+        public FactKpiMeasurement FactKpiMeasurement { get; set; } = null!;
     }
 }
