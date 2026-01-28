92390f4b840f4823cc6e61055ca9008f7673cd88
lskld <theo.lillieskold@gmail.com>
2026-01-28 10:39:47 +0100

    Create GenderCode Enum and added all attributes to the Gender Dimension

--- Munilytics.Server/Domain/Entities/DimGender.cs
+++ Munilytics.Server/Domain/Entities/DimGender.cs
@@ -1,6 +1,13 @@
-﻿namespace Munilytics.Server.Domain.Entities
+﻿using Munilytics.Server.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Munilytics.Server.Domain.Entities
 {
     public class DimGender
     {
+        [Key]
+        public GenderCode Code { get; set; }
+        public string GenderName { get; set; } = string.Empty;
+        public int SortOrder { get; set; } 
     }
 }
