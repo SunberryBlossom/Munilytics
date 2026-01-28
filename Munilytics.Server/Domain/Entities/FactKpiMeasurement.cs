54156ed7566a8dd2fa03ab598fe610b7792034c9
lskld <theo.lillieskold@gmail.com>
2026-01-28 11:43:35 +0100

    Added attributes to the Fact Table

--- Munilytics.Server/Domain/Entities/FactKpiMeasurement.cs
+++ Munilytics.Server/Domain/Entities/FactKpiMeasurement.cs
@@ -2,5 +2,17 @@
 {
     public class FactKpiMeasurement
     {
+        //Foreign Keys to all Dimension Tables
+        public int DimMunicipality { get; set; }
+        public int DimKPI { get; set; }
+        public int DimGender { get; set; }
+        public int DimTime { get; set; }
+
+
+        public decimal Value { get; set; }
+        public bool IsDefinite { get; set; }
+        public int Count { get; set; }
+        public DateTime ImportDate { get; set; }
+        public DateTime LatestUpdate { get; set; }
     }
 }
