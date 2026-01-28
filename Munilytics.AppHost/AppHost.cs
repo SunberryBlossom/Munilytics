3ab75a491c8a15cb987d3a84cb351bcfac523b92
Johannes Flodin <jannis020609@gmail.com>
2026-01-28 13:59:11 +0100

    trying to stack

--- Munilytics.AppHost/AppHost.cs
+++ Munilytics.AppHost/AppHost.cs
@@ -26,7 +26,6 @@ var cube = builder.AddContainer("cube", "cubejs/cube")
     .WithHttpEndpoint(port: 4000, targetPort: 4000, name: "http")
     .WithEnvironment("CUBEJS_DEV_MODE", "true")
     .WithEnvironment("CUBEJS_WEB_SOCKETS", "true")
-    // Needs to be changed for prod
     .WithEnvironment("CUBEJS_API_SECRET", "mysupersecret")
     // Postgres Integration
     .WithEnvironment("CUBEJS_DB_TYPE", "postgres")
@@ -35,9 +34,8 @@ var cube = builder.AddContainer("cube", "cubejs/cube")
     .WithEnvironment("CUBEJS_DB_NAME", "MunilyticsDb")
     .WithEnvironment("CUBEJS_DB_USER", "postgres")
     .WithEnvironment("CUBEJS_DB_PASS", postgresPassword)
-    // Uses memory for queues as per new standard
+    // Redis Integration
     .WithEnvironment("CUBEJS_CACHE_AND_QUEUE_DRIVER", "memory")
-    // Redis url for caching data results
     .WithEnvironment("CUBEJS_REDIS_URL", ReferenceExpression.Create($"redis://{redis.Resource.Name}:6379"))
     // Cubejs folder
     .WithBindMount("../cube", "/cube/conf")
