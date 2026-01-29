7f948eeabff16b5804285aab43efe5cfbe5355ff
lskld <theo.lillieskold@gmail.com>
2026-01-29 15:52:16 +0100

    Create Kolada Client and inject http client

---
+++ Munilytics.Server/Features/KoladaService/KoladaClient.cs
@@ -0,0 +1,13 @@
+﻿namespace Munilytics.Server.Features.KoladaService
+{
+    public class KoladaClient
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://api.kolada.se/v2/";
+
+        public KoladaClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
