621166d6f619d70bebc14a5eb21fcc34a0d4beba
lskld <theo.lillieskold@gmail.com>
2026-01-29 16:19:55 +0100

    Created method to GET data from the kolada API using its base URL + additional endpoint, and then deserialize the JSON data into a chosen type.

--- Munilytics.Server/Features/KoladaService/KoladaClient.cs
+++ Munilytics.Server/Features/KoladaService/KoladaClient.cs
@@ -1,4 +1,6 @@
-﻿namespace Munilytics.Server.Features.KoladaService
+﻿using System.Text.Json;
+
+namespace Munilytics.Server.Features.KoladaService
 {
     public class KoladaClient
     {
@@ -8,6 +10,18 @@
         public KoladaClient(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        /// <summary>
+        /// Retrieves data from the Kolada API and deserializes the JSON response into the specified type T.
+        /// </summary>
+        /// <typeparam name="T">The type into which the JSON response is deserialized.</typeparam>
+        /// <param name="endpoint">The relative URL of the endpoint to retrieve data from. This value should not include the base URL.</param>
+        /// <returns>An instance of type T with the deserialized data from the endpoint.</returns>
+        private async Task<T> GetAsync<T>(string endpoint, CancellationToken ct)
+        {
+            var json = await _httpClient.GetStringAsync(BaseUrl + endpoint, ct);
+            return JsonSerializer.Deserialize<T>(json)!;
         }
     }
 }
