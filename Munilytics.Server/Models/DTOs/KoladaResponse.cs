1a11522cb795b1c70051bf7f4c9af127f90faa36
lskld <theo.lillieskold@gmail.com>
2026-02-01 15:56:29 +0100

    Created DTO that reads the "values" object from Kolada API JSON, since almost all data objects from the API are inside the "values" list.

---
+++ Munilytics.Server/Models/DTOs/KoladaResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Munilytics.Server.Models.DTOs
+{
+    /// <summary>
+    /// Generic DTO that can be used for almost all Kolada requests, since almost all data is
+    /// wrapped in "value" objects.
+    /// </summary>
+    public record KoladaResponse<T>(
+        [property: JsonPropertyName("values")] List<T> Values
+    );
+}
