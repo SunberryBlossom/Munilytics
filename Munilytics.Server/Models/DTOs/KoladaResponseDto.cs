6caa2864541dd227b9f3e2aa8eed2f2399c1504c
lskld <theo.lillieskold@gmail.com>
2026-02-01 15:58:25 +0100

    Changed the name to end with Dto to make it more clear when used in other classes

--- Munilytics.Server/Models/DTOs/KoladaResponseDto.cs
+++ Munilytics.Server/Models/DTOs/KoladaResponseDto.cs
@@ -6,7 +6,7 @@ namespace Munilytics.Server.Models.DTOs
     /// Generic DTO that can be used for almost all Kolada requests, since almost all data is
     /// wrapped in "value" objects.
     /// </summary>
-    public record KoladaResponse<T>(
+    public record KoladaResponseDto<T>(
         [property: JsonPropertyName("values")] List<T> Values
     );
 }
