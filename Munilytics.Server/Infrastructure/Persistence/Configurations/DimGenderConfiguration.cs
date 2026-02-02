901be0a6714e0e475c848a4281dbb8c184c84098
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-02-02 11:22:59 +0100

    Add Relation and requirements in fluent API for dimgender

--- Munilytics.Server/Infrastructure/Persistence/Configurations/DimGenderConfiguration.cs
+++ Munilytics.Server/Infrastructure/Persistence/Configurations/DimGenderConfiguration.cs
@@ -1,6 +1,22 @@
-﻿namespace Munilytics.Server.Infrastructure.Persistence.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Munilytics.Server.Domain.Entities;
+
+namespace Munilytics.Server.Infrastructure.Persistence.Configurations
 {
-    public class DimGenderConfiguration
+    public class DimGenderConfiguration : IEntityTypeConfiguration<DimGender>
     {
+        public void Configure(EntityTypeBuilder<DimGender> builder)
+        {
+            builder.HasKey(dg => dg.Code);
+
+            builder.Property(dg => dg.GenderName).IsRequired();
+            builder.Property(dg => dg.SortOrder).IsRequired();
+
+            builder
+                .HasMany(dg => dg.FactKpiMeasurements)
+                .WithOne(fkm => fkm.DimGender)
+                .HasForeignKey(fkm => fkm.DimGenderId);
+        }
     }
 }
