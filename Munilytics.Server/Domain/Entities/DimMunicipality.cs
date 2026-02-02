257b485a0388c3b28ec68ee887f17b967f021990
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-02-02 11:41:06 +0100

    Correct spelling error

--- Munilytics.Server/Domain/Entities/DimMunicipality.cs
+++ Munilytics.Server/Domain/Entities/DimMunicipality.cs
@@ -6,8 +6,8 @@
         public string Title { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
         public string County { get; set; } = string.Empty;
-        public string SKRGroupCode { get; set; } = string.Empty;
-        public string SKRGroupName { get; set; } = string.Empty;
+        public string SkrGroupCode { get; set; } = string.Empty;
+        public string SkrGroupName { get; set; } = string.Empty;
 
         public ICollection<FactKpiMeasurement> FactKpiMeasurements { get; set; } = new List<FactKpiMeasurement>();
     }
