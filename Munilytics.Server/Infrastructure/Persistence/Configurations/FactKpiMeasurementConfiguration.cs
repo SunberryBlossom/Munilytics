10c84f31070616f0cba5b29d8a2022fcc26f9c69
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-02-02 11:53:36 +0100

    Add Relation and requirements in fluent API for Fact table

--- Munilytics.Server/Infrastructure/Persistence/Configurations/FactKpiMeasurementConfiguration.cs
+++ Munilytics.Server/Infrastructure/Persistence/Configurations/FactKpiMeasurementConfiguration.cs
@@ -1,6 +1,42 @@
-﻿namespace Munilytics.Server.Infrastructure.Persistence.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Munilytics.Server.Domain.Entities;
+
+namespace Munilytics.Server.Infrastructure.Persistence.Configurations
 {
-    public class FactKpiMeasurementConfiguration
+    public class FactKpiMeasurementConfiguration : IEntityTypeConfiguration<FactKpiMeasurement>
     {
+        public void Configure(EntityTypeBuilder<FactKpiMeasurement> builder)
+        {
+            builder.HasKey(kpm => kpm.Id);
+
+            builder.Property(fkm => fkm.Value).IsRequired();
+            builder.Property(fkm => fkm.IsDefinite).IsRequired();
+            builder.Property(fkm => fkm.Count).IsRequired();
+            builder.Property(fkm => fkm.ImportDate).IsRequired();
+            builder.Property(fkm => fkm.LatestUpdate).IsRequired();
+
+            builder
+                .HasOne(fkm => fkm.DimMunicipality)
+                .WithMany(dm => dm.FactKpiMeasurements)
+                .HasForeignKey(fkm => fkm.DimMunicipalityId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            builder
+                .HasOne(fkm => fkm.DimKpi)
+                .WithMany(dk => dk.FactKpiMeasurements)
+                .HasForeignKey(fkm => fkm.DimKpiId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(fkm => fkm.DimGender)
+                .WithMany(dg => dg.FactKpiMeasurements)
+                .HasForeignKey(fkm => fkm.DimGenderId);
+
+            builder
+                .HasOne(fkm => fkm.DimTime)
+                .WithMany(dt => dt.FactKpiMeasurements)
+                .HasForeignKey(fkm => fkm.DimTimeId);
+        }
     }
 }
