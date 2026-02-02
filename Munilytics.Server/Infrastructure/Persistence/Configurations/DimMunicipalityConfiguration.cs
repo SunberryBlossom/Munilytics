061a24c8e87da2c0a7d079cd3585fa7c98892384
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-02-02 11:41:16 +0100

    Add Relation and requirements in fluent API for dimMunicipality

--- Munilytics.Server/Infrastructure/Persistence/Configurations/DimMunicipalityConfiguration.cs
+++ Munilytics.Server/Infrastructure/Persistence/Configurations/DimMunicipalityConfiguration.cs
@@ -1,6 +1,25 @@
-﻿namespace Munilytics.Server.Infrastructure.Persistence.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Munilytics.Server.Domain.Entities;
+
+namespace Munilytics.Server.Infrastructure.Persistence.Configurations
 {
-    public class DimMunicipalityConfiguration
+    public class DimMunicipalityConfiguration :IEntityTypeConfiguration<DimMunicipality>
     {
+        public void Configure(EntityTypeBuilder<DimMunicipality> builder)
+        {
+            builder.HasKey(dm => dm.Id);
+
+            builder.Property(dm => dm.Title).HasMaxLength(200).IsRequired();
+            builder.Property(dm => dm.Type).HasMaxLength(200).IsRequired();
+            builder.Property(dm => dm.County).HasMaxLength(200).IsRequired();
+            builder.Property(dm => dm.SkrGroupCode).HasMaxLength(200).IsRequired();
+            builder.Property(dm => dm.SkrGroupName).HasMaxLength(200).IsRequired();
+
+            builder
+                .HasMany(dm => dm.FactKpiMeasurements)
+                .WithOne(fkm => fkm.DimMunicipality)
+                .HasForeignKey(fkm => fkm.DimMunicipalityId);
+        }
     }
 }
