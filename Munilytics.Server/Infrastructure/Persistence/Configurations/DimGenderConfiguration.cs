102121349a80bab5f693929f9cfee67100de5178
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2026-02-02 14:25:13 +0100

    Add seed data

--- Munilytics.Server/Infrastructure/Persistence/Configurations/DimGenderConfiguration.cs
+++ Munilytics.Server/Infrastructure/Persistence/Configurations/DimGenderConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Munilytics.Server.Domain.Entities;
+using Munilytics.Server.Domain.Enums;
 
 namespace Munilytics.Server.Infrastructure.Persistence.Configurations
 {
@@ -8,8 +9,9 @@ namespace Munilytics.Server.Infrastructure.Persistence.Configurations
     {
         public void Configure(EntityTypeBuilder<DimGender> builder)
         {
-            builder.HasKey(dg => dg.Code);
+            builder.HasKey(dg => dg.Id);
 
+            builder.Property(dg => dg.Code).HasMaxLength(1).HasConversion<string>().IsRequired(); // Makes certain that the db saves it as a char and not the index of the enum, ergo T instead of 0, for readability.
             builder.Property(dg => dg.GenderName).IsRequired();
             builder.Property(dg => dg.SortOrder).IsRequired();
 
@@ -17,6 +19,12 @@ namespace Munilytics.Server.Infrastructure.Persistence.Configurations
                 .HasMany(dg => dg.FactKpiMeasurements)
                 .WithOne(fkm => fkm.DimGender)
                 .HasForeignKey(fkm => fkm.DimGenderId);
+
+            builder.HasData(
+                new DimGender { Id = 1, Code = GenderCode.T, GenderName = "Total", SortOrder = 1 },
+                new DimGender { Id = 2, Code = GenderCode.F, GenderName = "Female", SortOrder = 2 },
+                new DimGender { Id = 3, Code = GenderCode.M, GenderName = "Male", SortOrder = 3 }
+            );
         }
     }
 }
