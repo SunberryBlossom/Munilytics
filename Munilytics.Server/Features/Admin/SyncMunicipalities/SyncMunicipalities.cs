2e7e3c0b4cd8becb05b3672969ea05c2d448e69f
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-02-05 14:03:14 +0100

    Add missing property of kolada api Id

--- Munilytics.Server/Features/Admin/SyncMunicipalities/SyncMunicipalities.cs
+++ Munilytics.Server/Features/Admin/SyncMunicipalities/SyncMunicipalities.cs
@@ -67,6 +67,7 @@ namespace Munilytics.Server.Features.Admin.SyncMunicipalities
                     {
                         existingIdentity.Title = dto.Title;
                         existingIdentity.Type = dto.Type;
+                        existingIdentity.KoladaId = dto.Id;
                     }
                 }
                 else
@@ -74,7 +75,8 @@ namespace Munilytics.Server.Features.Admin.SyncMunicipalities
                     var newEntity = new DimMunicipality
                     {
                         Title = dto.Title,
-                        Type = dto.Type
+                        Type = dto.Type,
+                        KoladaId = dto.Id
                     };
 
                     db.Dim_Municipalities.Add(newEntity);
