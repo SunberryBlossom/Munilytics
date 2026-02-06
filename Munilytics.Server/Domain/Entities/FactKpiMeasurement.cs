5d765358c6099a0fe29fe9c626b1fd5e1c03c580
lskld <theo.lillieskold@gmail.com>
2026-02-06 11:59:23 +0100

    Chaned IsDefinite to "Status" which is the name in Kolada, for easier mapping. Also changed to string because Kolada shows "" or "Missing"

--- Munilytics.Server/Domain/Entities/FactKpiMeasurement.cs
+++ Munilytics.Server/Domain/Entities/FactKpiMeasurement.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public decimal Value { get; set; }
-        public bool IsDefinite { get; set; }
+        public string Status { get; set; } = string.Empty;
         public int Count { get; set; }
         public DateTime ImportDate { get; set; }
         public DateTime LatestUpdate { get; set; }
