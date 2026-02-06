1da4dfcc3bc7a94fd23876bdfc4918df9f412d48
lskld <theo.lillieskold@gmail.com>
2026-02-06 15:55:57 +0100

    Fix another migration for nullable Status

---
+++ Munilytics.Server/Migrations/20260206145508_SetStatusNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Munilytics.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetStatusNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Fact_KpiMeasurements",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Fact_KpiMeasurements",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
