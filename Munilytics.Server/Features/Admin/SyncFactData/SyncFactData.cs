a8668e905ec0177cc744715c1dc97eb000e410f7
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-02-09 14:10:07 +0100

    Optimize ETL for fact table
    This commit makes sure the ETL is optimized for columnar structure, since we now have the Hydra extension applied to the fact table.
    I also added descriptive comments that can later be omitted (when going from development to release for example), but for now they're good for educational purposes.
    The big difference is that we now do a delete-and-insert instead of updating, since Hydra is append only.
    
    Message me if you have any questions about this commit

--- Munilytics.Server/Features/Admin/SyncFactData/SyncFactData.cs
+++ Munilytics.Server/Features/Admin/SyncFactData/SyncFactData.cs
@@ -40,124 +40,108 @@ namespace Munilytics.Server.Features.Admin.SyncFactData
     public static class SyncFactHandler
     {
         [Transactional]
-        public static async Task Handle(SyncFactCommand cmd, CancellationToken ct, MunilyticsDbContext db, ILogger logger, IKoladaService koladaService)
+public static async Task Handle(SyncFactCommand cmd, CancellationToken ct, MunilyticsDbContext db, ILogger logger, IKoladaService koladaService)
+{
+    // ------------------------- ETL FOR FACT TABLE -----------------------------
+
+    logger.LogInformation("Starting Fact sync for year: {Year}. Current batch: {BatchSize}", cmd.Year, cmd.Kpis.Length);
+
+    // EXTRACT
+    // Fetches the new facts from Kolada API into a list of DTOs. Checks to see if this was successful.
+    var newFacts = await koladaService.GetFactAsync<KoladaFactDto>(cmd.Kpis, cmd.Year.ToString(), ct);
+    if (newFacts is null || newFacts.Count == 0)
+    {
+        logger.LogWarning("No facts found for in {Year}. Skipping...", cmd.Year);
+        return;
+    }
+
+    // Mapping out municipalities, KPIs and Genders to dictionaries from our data warehouse (DWH)
+    var municipalitiesMap = await db.Dim_Municipalities.ToDictionaryAsync(m => m.KoladaId, m => m.Id, ct);
+
+    var kpisMap = await db.Dim_KPIs
+        .Where(k => cmd.Kpis.Contains(k.KpiCode))
+        .ToDictionaryAsync(k => k.KpiCode, k => k.Id, ct);
+
+    var gendersList = await db.Dim_Gender.ToListAsync(ct);
+    var gendersMap = gendersList.ToDictionary(g => g.Code.ToString(), g => g.Id);
+
+    // fetch the time ID for the year we are currently working with
+    var dimTimeId = await db.Dim_Time
+        .Where(t => t.Year == cmd.Year)
+        .Select(t => t.Id)
+        .FirstOrDefaultAsync(ct);
+
+    var now = DateTime.UtcNow;
+
+    if (dimTimeId == 0)
+    {
+        logger.LogError("Unknown year in db: {Year}. Is your database updated with the latest seed data?", cmd.Year);
+        return;
+    }
+
+    // this is because we use Columnar on the fact table:
+    // first make an array of the KPI Ids that we already have in our DWH, then delete those KPIs from it, for the current year that we are processing.
+    var kpiIdsToClear = kpisMap.Values.ToArray();
+    await db.Database.ExecuteSqlRawAsync(
+        "DELETE FROM \"Fact_KpiMeasurements\" WHERE \"DimTimeId\" = {0} AND \"DimKpiId\" = ANY({1})",
+        new object[] { dimTimeId, kpiIdsToClear }, ct);
+
+
+    // Create a list to hold all the new entities we wanna add into the DWH
+    var newEntities = new List<FactKpiMeasurement>(newFacts.Count);
+
+    // Go through each and every DTO that holds the new facts we fetched from Kolada...
+    foreach (var dto in newFacts)
+    {
+        // ...If the municipality, or the KPI is unknown, skip the DTO and go to the next.
+        if (!municipalitiesMap.TryGetValue(dto.MunicipalityKoladaId, out var municipalityId))
         {
-            logger.LogInformation("Starting Fact sync for year: {Year}. Current batch: {BatchSize}", cmd.Year, cmd.Kpis.Length);
-
-            // Get facts for current batch and current year for all municipalities
-            var newFacts = await koladaService.GetFactAsync<KoladaFactDto>(cmd.Kpis, cmd.Year.ToString(), ct);
-
-            if (newFacts is null || newFacts.Count == 0)
-            {
-                logger.LogWarning("No facts found for in {Year}. Skipping...", cmd.Year);
-                return;
-            }
-
-            // Map all inputs needed for later.
-            var municipalitiesMap = await db.Dim_Municipalities
-                .ToDictionaryAsync(m => m.KoladaId, m => m.Id, ct);
-
-            var kpisMap = await db.Dim_KPIs
-                .Where(k => cmd.Kpis.Contains(k.KpiCode))
-                .ToDictionaryAsync(k => k.KpiCode, k => k.Id, ct);
-
-            var gendersMap = await db.Dim_Gender
-                .ToDictionaryAsync(g => g.Code, g => g.Id, ct);
-
-            var dimTimeId = await db.Dim_Time
-                .Where(t => t.Year == cmd.Year)
-                .Select(t => t.Id)
-                .FirstOrDefaultAsync(ct);
-
-            if (dimTimeId == 0)
-            {
-                logger.LogError("Unknown year in db: {Year}. Is your database updated with the latest seed data?", cmd.Year);
-                return;
-            }
-
-            // Load existing facts for this year and for this kpi batch
-            var existingFacts = await db.Fact_KpiMeasurements
-                .Where(f => f.DimTimeId == dimTimeId && kpisMap.Values.Contains(f.DimKpiId))
-                .ToListAsync();
-
-            // Lookup table where we use composite key of municipality, kpiid and genderid to get a fact entity
-            var factMap = existingFacts
-                .ToDictionary(f => (MunicipalityId: f.DimMunicipalityId, KpiId: f.DimKpiId, GenderId: f.DimGenderId), f => f);
-
-            // Use a list to bulk add to dbcontext
-            var newEntities = new List<FactKpiMeasurement>();
-
-            foreach (var dto in newFacts)
-            {
-                // Skip this if this municipality or koladaid doesnt exist in our database
-                // Otherwise we give back the specific id for both.
-                // These will log, so can be a good idea to add the logged ones in the future
-                if (!municipalitiesMap.TryGetValue(dto.MunicipalityKoladaId, out var municipalityId))
-                {
-                    logger.LogDebug("Skipping Unknown Municipality: {MunicipalityId}", dto.MunicipalityKoladaId);
-                    continue;
-                }
-
-                if (!kpisMap.TryGetValue(dto.KpiKoladaId, out var kpiId))
-                {
-                    logger.LogTrace("Skipping unseeded KPI: {KpiId}", dto.KpiKoladaId);
-                    continue;
-                }
-
-                var genderString = string.IsNullOrWhiteSpace(dto.Gender) ? "T" : dto.Gender;
-
-                if (!Enum.TryParse<GenderCode>(genderString, true, out var genderCode) ||
-                    !gendersMap.TryGetValue(genderCode, out var genderId))
-                {
-                    logger.LogWarning("Unknown gender code '{Gender}'.", dto.Gender);
-                    continue;
-                }
-
-                var factKey = (MunicipalityId: municipalityId, KpiId: kpiId, GenderId: genderId);
-
-                if (factMap.TryGetValue(factKey, out var existingEntity))
-                {
-                    bool hasChanged =
-                        existingEntity.Value != (decimal)dto.Value ||
-                        existingEntity.Count != dto.Count ||
-                        existingEntity.Status != dto.Status;
-
-                    if (hasChanged)
-                    {
-                        existingEntity.Value = (decimal)dto.Value;
-                        existingEntity.Count = dto.Count;
-                        existingEntity.Status = dto.Status;
-
-                        existingEntity.LatestUpdate = DateTime.UtcNow;
-                    }
-                }
-                else
-                {
-                    var newEntity = new FactKpiMeasurement
-                    {
-                        Value = dto.Value,
-                        Count = dto.Count,
-                        Status = dto.Status,
-
-                        // FKs
-                        DimMunicipalityId = municipalityId,
-                        DimKpiId = kpiId,
-                        DimGenderId = genderId,
-                        DimTimeId = dimTimeId,
-
-                        ImportDate = DateTime.UtcNow,
-                        LatestUpdate = DateTime.UtcNow,
-                    };
-                    newEntities.Add(newEntity);
-                }
-            }
-            if (newEntities.Count != 0)
-            {
-                await db.Fact_KpiMeasurements.AddRangeAsync(newEntities);
-            }
-
-            logger.LogInformation("Finished Fact sync for year: {Year} with batch size {BatchSize}", cmd.Year, cmd.Kpis.Length);
+            logger.LogDebug("Skipping Unknown Municipality: {MunicipalityId}", dto.MunicipalityKoladaId);
+            continue;
         }
+
+        if (!kpisMap.TryGetValue(dto.KpiKoladaId, out var kpiId))
+        {
+            logger.LogTrace("Skipping unseeded KPI: {KpiId}", dto.KpiKoladaId);
+            continue;
+        }
+
+        // fetch a genderKey. If the DTOs gender has nothing in it, set it as T. Otherwise, use it as it was (most likely T, K or M instead of omitted)
+        var genderKey = string.IsNullOrWhiteSpace(dto.Gender) ? "T" : dto.Gender;
+
+        // checks if the database DOES NOT have the gendercode provided from the DTO (even after the ternary), log a warning and skip...
+        if (!gendersMap.TryGetValue(genderKey, out var genderId))
+        {
+            logger.LogWarning("Unknown gender code '{Gender}'.", dto.Gender);
+            continue;
+        }
+
+        // TRANSFORM
+        // Otherwise, create a new fact for the database
+        newEntities.Add(new FactKpiMeasurement
+        {
+            Value = (decimal)dto.Value,
+            Count = dto.Count,
+            Status = dto.Status,
+            DimMunicipalityId = municipalityId,
+            DimKpiId = kpiId,
+            DimGenderId = genderId,
+            DimTimeId = dimTimeId,
+            ImportDate = now,
+            LatestUpdate = now
+        });
+    }
+
+    // LOAD
+    // Final security check: This block only runs if we actually added something to our list of new entities.
+    if (newEntities.Count != 0)
+    {
+        await db.Fact_KpiMeasurements.AddRangeAsync(newEntities, ct);
+    }
+
+    // Final log to tell us everything worked
+    logger.LogInformation("Finished Fact sync for year: {Year}. Added {Count} records.", cmd.Year, newEntities.Count);
+}
     }
 
     public static class SyncAllFactsHandler
